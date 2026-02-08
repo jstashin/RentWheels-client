a257c9593842b9aa162131fe68f3ef1d177a27e7
Jerin Sayma <jstashin2021@gmail.com>
2026-02-08 16:01:40 +0600

    firebase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAC8QxlMC_-e0penHTwbMOXT-xNLsieA8Y",
-  authDomain: "rentwheels-client-346db.firebaseapp.com",
-  projectId: "rentwheels-client-346db",
-  storageBucket: "rentwheels-client-346db.firebasestorage.app",
-  messagingSenderId: "730904181757",
-  appId: "1:730904181757:web:6b3679237876b8d2dd3794"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain ,
+  projectId:import.meta.env.VITE_projectId ,
+  storageBucket:import.meta.env.VITE_storageBucket ,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId ,
+  appId:import.meta.env.VITE_appId,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
